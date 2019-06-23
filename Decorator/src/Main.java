bef81db54afd94f1c1d34a5aa3fe107b88db997c
Chuangzw <1668207735@qq.com>
2019-06-23 15:26:46 +0800

    0.0.2

--- Decorator/src/Main.java
+++ Decorator/src/Main.java
@@ -9,33 +9,34 @@ public class Main {
         TShirts dtx = new TShirts();
 
         pqx.Decorate(xc);
+        pqx.Show();
         kk.Decorate(pqx);
         dtx.Decorate(kk);
         dtx.Show();
 
-        System.out.println("第二种装扮：");
-
-        LeatherShoes px = new LeatherShoes();
-        Tie ld = new Tie();
-        Suit xz = new Suit();
-
-        px.Decorate(xc);
-        ld.Decorate(px);
-        xz.Decorate(ld);
-        xz.Show();
-
-
-        System.out.println("第三种装扮：");
-        Sneaker pqx2 = new Sneaker();
-        LeatherShoes px2 = new LeatherShoes();
-        BigTrouser kk2 = new BigTrouser();
-        Tie ld2 = new Tie();
-
-        pqx2.Decorate(xc);
-        px2.Decorate(pqx);
-        kk2.Decorate(px2);
-        ld2.Decorate(kk2);
-
-        ld2.Show();
+//        System.out.println("第二种装扮：");
+//
+//        LeatherShoes px = new LeatherShoes();
+//        Tie ld = new Tie();
+//        Suit xz = new Suit();
+//
+//        px.Decorate(xc);
+//        ld.Decorate(px);
+//        xz.Decorate(ld);
+//        xz.Show();
+//
+//
+//        System.out.println("第三种装扮：");
+//        Sneaker pqx2 = new Sneaker();
+//        LeatherShoes px2 = new LeatherShoes();
+//        BigTrouser kk2 = new BigTrouser();
+//        Tie ld2 = new Tie();
+//
+//        pqx2.Decorate(xc);
+//        px2.Decorate(pqx);
+//        kk2.Decorate(px2);
+//        ld2.Decorate(kk2);
+//
+//        ld2.Show();
     }
 }
--- Facade/src/SubSystem.java
+++ Facade/src/SubSystem.java
@@ -1,5 +1,3 @@
-public class SubSystem {
-}
 class SubSystemOne{
     public void MethodOne(){
         System.out.println("子系统方法一");
